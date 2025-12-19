628182211f6cb8c74d5bca6763e1ade35cacc3c6
dk6502 <dk6502@penis.com>
2025-12-18 22:27:36 -0600

    serial port debugging

--- kernel/src/main.rs
+++ kernel/src/main.rs
@@ -7,15 +7,11 @@ const CHAR_HEIGHT: usize = 24;
 use core::arch::asm;
 use core::fmt::{self, Write};
 
-use lazy_static::lazy_static;
 use limine::BaseRevision;
 use limine::framebuffer::Framebuffer;
 use limine::request::{FramebufferRequest, RequestsEndMarker, RequestsStartMarker};
 use spleen_font::{FONT_12X24, PSF2Font};
-use x86_64::instructions::tables::load_tss;
-use x86_64::registers::segmentation::{CS, SS, Segment};
-use x86_64::structures::gdt::{self, Descriptor, GlobalDescriptorTable, SegmentSelector};
-use x86_64::structures::tss::TaskStateSegment;
+use uart_16550::SerialPort;
 
 // #[used] lets the compiler know not to remove
 #[used]
@@ -34,11 +30,7 @@ static _START_MARKER: RequestsStartMarker = RequestsStartMarker::new();
 #[unsafe(link_section = ".requests_end_marker")]
 static _END_MARKER: RequestsEndMarker = RequestsEndMarker::new();
 
-struct Selectors {
-  code_selector: SegmentSelector,
-  tss_selector: SegmentSelector,
-  data_selector: SegmentSelector,
-}
+const SERIAL_IO_PORT: u16 = 0x3F8;
 
 trait Display {
   unsafe fn write_pixel(&self, code: u32, x: u64, y: u64);
@@ -59,7 +51,7 @@ impl<'a> Display for Framebuffer<'a> {
 
 struct Writer<'a> {
   framebuffer: Option<&'a Framebuffer<'a>>,
-  font: &'a mut PSF2Font<'a>,
+  font: Option<&'a mut PSF2Font<'a>>,
   col_x: usize,
   row_y: usize,
 }
@@ -68,7 +60,7 @@ impl<'a, 'b> Writer<'a>
 where
   'a: 'b,
 {
-  fn new(framebuffer: Option<&'a Framebuffer<'b>>, font: &'a mut PSF2Font<'b>) -> Self {
+  fn new(framebuffer: Option<&'a Framebuffer<'b>>, font: Option<&'a mut PSF2Font<'b>>) -> Self {
     Self {
       framebuffer,
       font,
@@ -87,7 +79,7 @@ impl<'a> fmt::Write for Writer<'a> {
         self.row_y += 1;
         self.col_x = 0;
       } else {
-        self.write_char(char);
+        let _ = self.write_char(char);
         self.col_x += 1;
       }
     }
@@ -96,26 +88,27 @@ impl<'a> fmt::Write for Writer<'a> {
 
   fn write_char(&mut self, text: char) -> Result<(), core::fmt::Error> {
     let mut tmp = [0u8, 2];
-
-    if let Some(glyph) = self
-      .font
-      .glyph_for_utf8(text.encode_utf8(&mut tmp).as_bytes())
-      && let Some(framebuffer) = self.framebuffer
+    let bytes = text.encode_utf8(&mut tmp).as_bytes();
+    if let Some(framebuffer) = self.framebuffer
+      && let Some(ref mut font) = self.font
     {
-      for (row_y, row) in glyph.enumerate() {
-        for (col_x, on) in row.enumerate() {
-          unsafe {
-            if on {
-              framebuffer.write_pixel(
-                0xFFFFFFFF,
-                (self.col_x * CHAR_WIDTH + col_x) as u64,
-                (self.row_y * CHAR_HEIGHT + row_y) as u64,
-              );
+      if let Some(glyph) = font.glyph_for_utf8(bytes) {
+        for (row_y, row) in glyph.enumerate() {
+          for (col_x, on) in row.enumerate() {
+            unsafe {
+              if on {
+                framebuffer.write_pixel(
+                  0xFFFFFFFF,
+                  (self.col_x * CHAR_WIDTH + col_x) as u64,
+                  (self.row_y * CHAR_HEIGHT + row_y) as u64,
+                );
+              }
             }
           }
         }
       }
     }
+
     Ok(())
   }
 }
@@ -123,17 +116,21 @@ impl<'a> fmt::Write for Writer<'a> {
 #[unsafe(no_mangle)]
 unsafe extern "C" fn kmain() -> ! {
   assert!(BASE_REVISION.is_supported());
+  let mut serial_port = unsafe { SerialPort::new(SERIAL_IO_PORT) };
+  let mut font = PSF2Font::new(FONT_12X24).unwrap();
+  serial_port.init();
   if let Some(framebuffer_response) = FRAMEBUFFER_REQUEST.get_response() {
     if let Some(framebuffer) = framebuffer_response.framebuffers().next() {
-      let mut font = PSF2Font::new(FONT_12X24).unwrap();
-      let mut writer = Writer::new(Some(&framebuffer), &mut font);
-      writeln!(writer, "dkos 0.0.1");
-      writeln!(writer, "{:?}", 67.0 / 61.0);
-      writeln!(writer, "{:?}", framebuffer.addr());
+      let mut writer = Writer::new(Some(&framebuffer), Some(&mut font));
+      let _ = writeln!(writer, "dkos 0.0.1");
+      let _ = writeln!(writer, "{:?}", 67.0 / 61.0);
+      let _ = writeln!(writer, "{:?}", framebuffer.addr());
     }
   }
   hcf();
 }
+
+unsafe fn iret() {}
 
 #[panic_handler]
 fn rust_panic(_info: &core::panic::PanicInfo) -> ! {
